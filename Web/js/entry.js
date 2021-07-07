4d3cb33480308223d793d23c44cf1bc27f04c15c
nannada4 <coco96062233@gmail.com>
2021-07-07 10:28:57 +0900

    changed class name, revised 3d effects

--- Web/js/entry.js
+++ Web/js/entry.js
@@ -3,11 +3,11 @@ const card = document.querySelector(".card");
 const EntryImage = document.querySelector(".EntryImage img");
 const title = document.querySelector(".title");
 const EntryTitle = document.querySelector(".EntryTitle");
-const sizes = document.querySelector(".sizes");
+const LinkTo = document.querySelector(".LinkTo");
 
 card.addEventListener("mousemove", function (e) {
     let xAxis = (window.innerWidth / 2 - e.pageX) / 60;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 30;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / -30;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
@@ -15,7 +15,7 @@ card.addEventListener("mouseenter", function (e) {
     card.style.transition = "none";
     title.style.transform = "translateZ(100px)";
     EntryTitle.style.transform = "translateZ(70px)";
-    sizes.style.transform = "translateZ(50px)";
+    LinkTo.style.transform = "translateZ(50px)";
 });
 
 card.addEventListener("mouseleave", function (e) {
@@ -23,7 +23,7 @@ card.addEventListener("mouseleave", function (e) {
     card.style.transform = "rotateX(0deg) rotateY(0deg)";
     title.style.transform = "translateZ(0px)";
     EntryTitle.style.transform = "translateZ(0px)";
-    sizes.style.transform = "translateZ(0px)";
+    LinkTo.style.transform = "translateZ(0px)";
 });
 
 card.addEventListener("mouseenter", (e) => {
